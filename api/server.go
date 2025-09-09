49de8462fd54b763333eecd6e3df05498c4a549d
kenk999 <mohammadhifzan24@gmail.com>
2025-09-09 23:52:18 +0530

    updated routes

---
+++ api/server.go
@@ -0,0 +1,12 @@
+package main 
+func (app *application) serve() error{
+	server := &http.Server{
+		Addr: fmt.Sprintf("%d", app.port),
+		Handler: app.routes(),
+		IdleTimeout: time.Minute,
+		ReadTimeout: 10*time.Second,
+		WriteTimeout: 30*time.Second,
+	}
+	log.Printf("starting server on port %d",app.port)
+	return server.ListenAndServe()
+}
